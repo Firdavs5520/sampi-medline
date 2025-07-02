c5407b6d9b1275f3b2564eabf0b6608f2c1ebc4d
Firdavs5520 <sharipovfirdavs5520@gmail.com>
2025-07-02 16:43:06 +0500

    map fixed

--- src/Components/Maps/Maps.jsx
+++ src/Components/Maps/Maps.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Maps = () => {
   return (
-    <div className="flex justify-center my-4">
+    <div className="flex justify-center my-4 sm:mx-44 mx-4">
       <iframe
         src="https://yandex.uz/map-widget/v1/?ll=69.3315%2C41.3649&z=17&l=map&pt=69.3315,41.3649,pm2rdm"
-        width="94.5%"
-        height="400"
+        width="100%"
+        height="350"
         frameBorder="0"
         allowFullScreen
         className="rounded-xl"
