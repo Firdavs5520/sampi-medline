2d03f9b3a0e07654a76d71fe4ad62eb8984b33fd
Firdavs5520 <sharipovfirdavs5520@gmail.com>
2025-07-02 09:54:02 +0500

    yakuniy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 // tailwind.config.js
 import withMT from "@material-tailwind/react/utils/withMT.js";
 
-/** @type {import('tailwindcss').Config} */
 export default withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
